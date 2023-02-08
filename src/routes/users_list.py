e7c03f4c3a1445ab61dbdd2fc28e9ccec5102e47
Jesus_B <17-10046@usb.ve>
2023-02-08 12:37:22 -0400

    Added functions to delete users.It still need backend

--- src/routes/users_list.py
+++ src/routes/users_list.py
@@ -1,8 +1,16 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
+from src.models.User import User
 from . import app
 
 @app.route('/users_list')
 def users_lists():
     return render_template(
-        'users_list/users_list.html'       
+        'users_list/users_list.html'
+        , users=[User("Coronao"), User("Chang"), User("Yriarte"), User("EB"), User("Fernando")] # Aqui va la lista de usuarios de la base de datos. Deberiamos agregar paginacion???   
     )
+
+@app.route('/users_list/delete', methods=['GET', 'POST'])
+def delete_user():
+    # Eliminar al usuario de la base
+    # TODO
+    return redirect(url_for('users_lists'))
