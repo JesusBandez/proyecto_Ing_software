4d124ba394c3f8c1bae1c8898544ae7f2e540cd2
Jesus_B <17-10046@usb.ve>
2023-02-08 14:25:47 -0400

    Added register user

--- src/routes/users_list.py
+++ src/routes/users_list.py
@@ -12,6 +12,19 @@ def users_lists():
 @app.route('/users_list/delete', methods=['GET', 'POST'])
 def delete_user():
     user_id = request.form['id']
-    # Eliminar al usuario de la base
+    # Eliminar al usuario de la base de datos
     # TODO
+    return redirect(url_for('users_lists'))
+
+@app.route('/users_list/new_user')
+def new_user():
+    return render_template('users_list/new_user.html')
+
+@app.route('/users_list/add_new_user', methods=['POST'])
+def add_new_user():
+    # Agregar el nuevo usuario a la base de datos
+    # TODO
+    print(request.form['username'])
+    print(request.form['password'])
+    print(request.form['permissions'])
     return redirect(url_for('users_lists'))
