e295f3b1e4ffb5baea68bf274df4dc92a3397c87
Jesus_B <17-10046@usb.ve>
2023-02-10 21:29:09 -0400

    added tests

--- tests/test.py
+++ tests/test.py
@@ -11,25 +11,7 @@ unittest.TestLoader.sortTestMethodsUsing = None
 # Estas pruebas son realizadas con el navegador Firefox. Se necesita
 # selenium y el driver geckodriver para poder iniciarlas
 
-
-
-
 class tests(unittest.TestCase):
-    def test_d_delete_user_admin(self):
-        '''Esta prueba requiere que exista un usuario con nombre 1,
-        contrasenia 1 y rol admin en la base de datos, y un usuario de nombre 3'''
-        print("Borrar usuario")
-        browser = webdriver.Firefox(executable_path="./drivers/geckodriver")
-        browser.get('http://127.0.0.1:5000/login')
-        browser.find_element('id', 'username').send_keys("1")
-        browser.find_element('id', 'password').send_keys("1")
-        sleep(1)
-        browser.find_element('name', 'submit').click()
-        self.assertEqual(browser.title, 'User\'s list' )
-        sleep(1)
-        browser.find_element(By.XPATH, r"// td[ text() = '3' ]/..").find_element(By.CSS_SELECTOR, r'button').click()
-        sleep(1)
-        browser.quit()
 
     def test_a_non_existent_user(self):
         print("Usuario no existente")
@@ -57,7 +39,7 @@ class tests(unittest.TestCase):
     def test_c_login(self):
         '''Esta prueba requiere que exista un usuario con nombre 1
         y contrasenia 1 en la base de datos'''
-        print("Login correcto")
+        print("Login y logout correcto")
         browser = webdriver.Firefox(executable_path="./drivers/geckodriver")
         browser.get('http://127.0.0.1:5000/login')
         browser.find_element('id', 'username').send_keys("1")
@@ -66,12 +48,66 @@ class tests(unittest.TestCase):
         browser.find_element('name', 'submit').click()
         self.assertEqual(browser.title, 'User\'s list' )
         sleep(3)
+        browser.find_element(By.CSS_SELECTOR, 'div.container-fluid>a.btn').click()
+        sleep(1)
+        browser.quit()
+        
+
+    def test_d_delete_user_admin(self):
+        '''Esta prueba requiere que exista un usuario con nombre 1,
+        contrasenia 1 y rol admin en la base de datos, y un usuario de nombre 3'''
+        print("Borrar usuario")
+        browser = webdriver.Firefox(executable_path="./drivers/geckodriver")
+        browser.get('http://127.0.0.1:5000/login')
+        browser.find_element('id', 'username').send_keys("1")
+        browser.find_element('id', 'password').send_keys("1")
+        sleep(1)
+        browser.find_element('name', 'submit').click()
+        self.assertEqual(browser.title, 'User\'s list' )
+        sleep(1)
+        browser.find_element(By.XPATH, r"// td[ text() = '3' ]/..").find_element(By.CSS_SELECTOR, r'button').click()
+        sleep(3)
+        browser.find_element(By.CSS_SELECTOR, 'div.container-fluid>a.btn').click()
         browser.quit()
 
+    def test_e_add_user_admin(self):
+        '''Esta prueba requiere que exista un usuario con nombre 1,
+        contrasenia 1 y rol admin en la base de datos, y un usuario de nombre 3'''
+        print("Agregar usuario borrado")
+        browser = webdriver.Firefox(executable_path="./drivers/geckodriver")
+        browser.get('http://127.0.0.1:5000/login')
+        browser.find_element('id', 'username').send_keys("1")
+        browser.find_element('id', 'password').send_keys("1")
+        sleep(1)
+        browser.find_element('name', 'submit').click()
+        self.assertEqual(browser.title, 'User\'s list' )
+        browser.find_element(By.CSS_SELECTOR, 'div.container-sm>a').click()
+        sleep(1)
+        browser.find_element('id', 'username').send_keys("3")
+        browser.find_element('id', 'password').send_keys("3")
+        sleep(2)
+        browser.find_element('name', 'submit').click()
+        self.assertEqual(browser.title, 'User\'s list' )
+        sleep(3)
+        browser.find_element(By.CSS_SELECTOR, 'div.container-fluid>a.btn').click()
+        browser.quit()
 
-
-
-
+    def test_f_non_admin_user(self):
+        '''Esta prueba requiere que exista un usuario con nombre 1,
+        contrasenia 1 y rol admin en la base de datos, y un usuario de nombre 3'''
+        print("Login usuario comun")
+        browser = webdriver.Firefox(executable_path="./drivers/geckodriver")
+        browser.get('http://127.0.0.1:5000/login')
+        browser.find_element('id', 'username').send_keys("4")
+        browser.find_element('id', 'password').send_keys("4")
+        sleep(1)
+        browser.find_element('name', 'submit').click()
+        self.assertEqual(browser.title, 'User\'s list' )
+        sleep(3)
+        browser.get("http://127.0.0.1:5000/users_list/new_user")
+        sleep(3)
+        browser.find_element(By.CSS_SELECTOR, 'div.container-fluid>a.btn').click()
+        browser.quit()
 
 if __name__ == "__main__":
     unittest.main()
